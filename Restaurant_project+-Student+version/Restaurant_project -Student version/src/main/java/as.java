6781f3d5eea6cfdad792c5790a3f4a4f08ad229a
Jackdawson47 <tailsanddreams@gmail.com>
2021-05-04 23:24:32 +0200

    Part 2:Solution

---
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/main/java/as.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class as {
+    public static void main(String[] args) {
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+
+    }
+}
